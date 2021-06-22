79f40ae4d83d17ee939aca5a2514686153033d1c
VictorPozzan <victor_pozzan@hotmail.com>
2021-06-22 09:14:38 -0300

    add prints

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ sub_String_A, sub_String_B = data.cleanData(file)
 
 bfrec = BFRecursiveLCS()
 size, time, comp, string =  bfrec.lcs(sub_String_A,sub_String_B)
+print("BRUTE FORCE RECURSIVE")
+print("size: ", size)
+print("time: ", time)
+print("comp: ", comp)
+print("string: ", string)
 
 bruteforceLCS = BruteForceLCS()
 size, time, comp, string= bruteforceLCS.lcs(sub_String_A, sub_String_B)
+print("BRUTE FORCE")
+print("size: ", size)
+print("time: ", time)
+print("comp: ", comp)
+print("string: ", string)
+
     
 dynamicLCS = DynamicLCS()
 size, time, comp, string = dynamicLCS.lcs(sub_String_A, sub_String_B)
+print("DYNAMIC")
+print("size: ", size)
+print("time: ", time)
+print("comp: ", comp)
+print("string: ", string)
 
 
