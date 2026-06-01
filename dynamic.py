b6e7bfc586447e6784af9f844c1c1003fed15213
VictorPozzan <victor_pozzan@hotmail.com>
2026-06-01 12:07:00 -0300

    refactor: streamline LCS implementation and enhance performance tracking

--- dynamic.py
+++ dynamic.py
@@ -1,50 +1,43 @@
+from time import perf_counter
 
-# Dynamic Programming implementation of LCS problem
-
-import time
 
 class DynamicLCS:
-    comp = 0
+    def __init__(self):
+        self.comp = 0
 
-    def lcs(self, X, Y):
-        start = time.time()
-        m = len(X)
-        n = len(Y)
-        L = [[0]*(n+1) for i in range(m+1)]
+    def lcs(self, string_a, string_b):
+        self.comp = 0
+        start = perf_counter()
 
-        for i in range(m+1):
-            for j in range(n+1):
-                if i == 0 or j == 0:
-                    L[i][j] = 0 
-                elif X[i-1] == Y[j-1]:
-                    self.comp = self.comp + 1
-                    L[i][j] = L[i-1][j-1]+1
+        rows = len(string_a)
+        cols = len(string_b)
+        table = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+        for row in range(1, rows + 1):
+            for col in range(1, cols + 1):
+                self.comp += 1
+                if string_a[row - 1] == string_b[col - 1]:
+                    table[row][col] = table[row - 1][col - 1] + 1
                 else:
-                    self.comp = self.comp + 1 
-                    L[i][j] = max(L[i-1][j], L[i][j-1])
+                    table[row][col] = max(table[row - 1][col], table[row][col - 1])
 
-        end = time.time()
-        executionTime = end - start
+        sub_string = self.findSubString(table, rows, cols, string_a, string_b)
+        execution_time = perf_counter() - start
 
-        sub_string = self.findSubString(L, m, n, X, Y)
+        return table[rows][cols], execution_time, self.comp, sub_string
 
-        return L[m][n],  executionTime, self.comp, sub_string
+    def findSubString(self, table, row, col, string_a, string_b):
+        lcs_chars = []
 
-    def findSubString(self, L,  m, n, X, Y):
-        index = L[m][n]
-        lsc = [""] * (index+1)
-        lsc[index] = ""
-        i = m
-        j = n
-        while i > 0 and j > 0:
-            if X[i-1] == Y[j-1]:
-                lsc[index-1] = X[i-1]
-                i -= 1
-                j -= 1
-                index -= 1
-            elif L[i-1][j] > L[i][j-1]:
-                i -= 1
+        while row > 0 and col > 0:
+            if string_a[row - 1] == string_b[col - 1]:
+                lcs_chars.append(string_a[row - 1])
+                row -= 1
+                col -= 1
+            elif table[row - 1][col] >= table[row][col - 1]:
+                row -= 1
             else:
-                j -= 1
+                col -= 1
 
-        return "".join(lsc)
+        lcs_chars.reverse()
+        return "".join(lcs_chars)
