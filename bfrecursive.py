411e6b1b587235fed4ee6ac90671b4938dd45c94
VictorPozzan <victor_pozzan@hotmail.com>
2026-06-01 12:06:39 -0300

    refactor: simplify LCS implementation and improve performance measurement

--- bfrecursive.py
+++ bfrecursive.py
@@ -1,47 +1,31 @@
+from time import perf_counter
 
-# Dynamic Programming implementation of LCS problem
-
-import time
 
 class BFRecursiveLCS:
-    comp = 0
+    def __init__(self):
+        self.comp = 0
 
-    def lcs(self, X, Y):
-        start = time.time()
-        m = len(X)
-        n = len(Y)
-        
-        size = self.lcs_recursive(X, Y, m, n)
-        end = time.time()
-        executionTime = end - start
+    def lcs(self, string_a, string_b):
+        self.comp = 0
+        start = perf_counter()
+        subsequence = self.lcs_recursive(string_a, string_b)
+        execution_time = perf_counter() - start
 
-        string = self.slcs(X,Y)
+        return len(subsequence), execution_time, self.comp, subsequence
 
-        return size, executionTime, self.comp, string
+    def lcs_recursive(self, string_a, string_b):
+        self.comp += 1
 
-    def lcs_recursive(self ,X, Y, m, n):
-        self.comp = self.comp + 2
-        if m == 0 or n == 0:
-            return 0;
-        elif X[m-1] == Y[n-1]:
-            self.comp = self.comp + 1
-            return 1 + self.lcs_recursive(X, Y, m-1, n-1)
-        else:
-            self.comp = self.comp + 1
-            return max(self.lcs_recursive(X, Y, m, n-1), self.lcs_recursive(X, Y, m-1, n))
-    
-
-    def slcs(self, str1, str2):
-        if len(str1) == 0 or len(str2) == 0:
+        if not string_a or not string_b:
             return ""
-        
-        if str1[-1] == str2[-1]:
-            return self.slcs(str1[:-1], str2[:-1]) + str1[-1]
-            
-        t1 = self.slcs(str1[:-1], str2)
-        t2 = self.slcs(str1, str2[:-1])
-        if len(t1) > len(t2):
-            return t1
-        else:
-            return t2
-    
+
+        if string_a[-1] == string_b[-1]:
+            return self.lcs_recursive(string_a[:-1], string_b[:-1]) + string_a[-1]
+
+        left = self.lcs_recursive(string_a[:-1], string_b)
+        right = self.lcs_recursive(string_a, string_b[:-1])
+
+        if len(left) >= len(right):
+            return left
+
+        return right
